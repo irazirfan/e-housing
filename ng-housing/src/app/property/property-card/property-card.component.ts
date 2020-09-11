85359af1cbffb201e1b5b2ef773cb2d8ef55d77f
Iraz Irfan <irairfan@gmail.com>
2020-09-11 06:13:11 +0600

    property-list with dmmy entries

--- ng-housing/src/app/property/property-card/property-card.component.ts
+++ ng-housing/src/app/property/property-card/property-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-property-card',
@@ -8,12 +8,6 @@ import { Component } from '@angular/core';
 
 
 export class PropertyCardComponent {
-
-Property: any = {
-  "Id": 1,
-  "Name":"Dream House",
-  "Type":"House",
-  "Price":12000
-}
+@Input() property : any
 
 }
