2dfa4bf5533cc6581e26ab6182bf4b0984968f80
Iraz Irfan <irazirfan@gmail.com>
2020-09-13 20:56:50 +0600

    routing init

--- ng-housing/src/app/property/property-card/property-card.component.ts
+++ ng-housing/src/app/property/property-card/property-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { IProperty } from '../IProperty.interface';
 
 @Component({
   selector: 'app-property-card',
@@ -8,6 +9,6 @@ import { Component, Input } from '@angular/core';
 
 
 export class PropertyCardComponent {
-@Input() property : any
+@Input() property : IProperty
 
 }
--- ng-housing/src/app/property/property-list/property-list.component.ts
+++ ng-housing/src/app/property/property-list/property-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HousingService } from 'src/app/service/housing.service';
+import { IProperty } from '../IProperty.interface';
 
 @Component({
   selector: 'app-property-list',
@@ -8,7 +9,7 @@ import { HousingService } from 'src/app/service/housing.service';
 })
 export class PropertyListComponent implements OnInit {
 
-  properties: any;
+  properties: Array<IProperty>;
 
   constructor(private housingService: HousingService) { }
 
@@ -18,6 +19,7 @@ export class PropertyListComponent implements OnInit {
         this.properties=data;
         console.log(data);
       }, error => {
+        console.log('httperror:');
         console.log(error);
       }
     )
