4ff966db61ff7bfe50ae7c346f1b56f05a6c5f2f
Iraz Irfan <irazirfan@gmail.com>
2021-02-17 01:46:17 +0600

    AccControlle, LoginResDto, LoginReqDto init

---
+++ webApi/Dtos/LoginReqDto.cs
@@ -0,0 +1,8 @@
+namespace webApi.Dtos
+{
+    public class LoginReqDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ webApi/Dtos/LoginResDto.cs
@@ -0,0 +1,8 @@
+namespace webApi.Dtos
+{
+    public class LoginResDto
+    {
+        public string Username { get; set; }
+        public string Token { get; set; }
+    }
+}
